b6989e9d4a3e822b55042b798eba484e1b94850d
Isaac Chettiath <ichettiath2019@g.ucla.edu>
2023-04-18 06:07:13 +0000

    Set up parsing a request and formulating a reply
    #What: Server now takes in client request, parses it, and generates a reply
    
    #Why: In the previous iteration, the server relayed the client's request back to them. A method of parsing the request and generating a response is needed.
    
    #How: Added header, reply, request, and request_parser files. Modified the session to take in data from the socket stream and generate a response from it.
    
    Change-Id: I1b9a9e3648c5a7a322565d12f70a63e37c37aa1f

--- include/session.h
+++ include/session.h
@@ -3,6 +3,8 @@
 
 #include <boost/bind.hpp>
 #include <boost/asio.hpp>
+#include "reply.h"
+#include "request_parser.h"
 
 using boost::asio::ip::tcp;
 
@@ -12,12 +14,15 @@ public:
   session(boost::asio::io_service& io_service);
   tcp::socket& socket();
   void start();
+  reply generate_response(char *data_, int bytes_transferred);
 private:
   void handle_read(const boost::system::error_code& error, size_t bytes_transferred);
   void handle_write(const boost::system::error_code& error);
   tcp::socket socket_;
   enum { max_length = 1024 };
   char data_[max_length];
+  reply reply_;
+  request_parser req_parser_;
 };
 
 #endif
--- src/server.cc
+++ src/server.cc
@@ -8,30 +8,31 @@ using boost::asio::ip::tcp;
 
 server::server(boost::asio::io_service& io_service, short port)
   : io_service_(io_service),
-    acceptor_(io_service, tcp::endpoint(tcp::v4(), port))
+    acceptor_(io_service, tcp::endpoint(tcp::v4(), port)) 
 {
   start_accept();
 }
 
 void server::start_accept()
 {
-  session* new_session = new session(io_service_);
-  acceptor_.async_accept(new_session->socket(),
+  session* new_session = new session(io_service_); //initializes a tcp socket in member var socket_
+  acceptor_.async_accept(new_session->socket(), //starts an asynchronous accept for incoming requests 
       boost::bind(&server::handle_accept, this, new_session,
         boost::asio::placeholders::error));
+      //bind(f, x, y) returns a function object with the value f(x,y)
 }
 
-void server::handle_accept(session* new_session,
+void server::handle_accept(session* new_session, //gets called by async_accept each time a request comes in 
     const boost::system::error_code& error)
 {
   if (!error)
   {
-    new_session->start();
+    new_session->start(); //session.cc: starts reading from socket stream
   }
   else
   {
     delete new_session;
   }
 
-  start_accept();
+  start_accept(); //once a request is fully processed, a new session is created to wait for next request
 }
