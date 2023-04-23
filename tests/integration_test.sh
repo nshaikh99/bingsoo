475c40a722a8d19016ffac3cf7bf38a4ca08d286
Muthu Palaniappan <muthupal@g.ucla.edu>
2023-04-23 23:42:35 +0000

    added integration test
    Change-Id: I7262a61639214cd63709cccfcd349ea7996316db

---
+++ tests/integration_test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Start the webserver; uses port 80
+./bin/server tests/test_configs/example_config &
+
+sleep 1
+# send a request to the server
+curl http://localhost:80 -s -o ./tests/result
+
+diff ./tests/result ./tests/expected_integration_test_result
+
+if [[ $? -eq 0 ]]; then
+    echo "success"; 
+    kill %1
+    rm ./tests/result
+    exit 0;
+else 
+    echo "failed"; 
+    kill %1
+    rm ./tests/result
+    exit 1;
+fi
