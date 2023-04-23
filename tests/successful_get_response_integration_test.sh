4a4762ccc89a8452cb80879fbfb80237611ffb8c
Muthu Palaniappan <muthupal@g.ucla.edu>
2023-04-23 23:42:35 +0000

    Added first integration test
    Added a bash integration test to the test suite. This sends a successful ping to our server with an empty body and is returned a successful response.
    This test uses curl and port 80.
    
    Allows us to test the entire process of running the server, sending a command, and receiving a response. Our unit tests tests our functions and code components, so this test
    is an additional layer that tests all the moving parts together. It is more general than a unit test.
    
    Used Bash and created a shell script; added a test rule into our CMAKE to run this integration test with `make test`.
    The integration test compares the output of the server response to a file
    with the standard expected response.
    
    Change-Id: I7262a61639214cd63709cccfcd349ea7996316db

---
+++ tests/successful_get_response_integration_test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Start the webserver; uses port 80
+./bin/server ../conf/sample.conf &
+
+sleep 1
+# send a request to the server
+curl -i -v -o ./tests/result http://localhost:80 -s
+
+
+diff ./tests/result ./tests/successful_get_response_integration_test_result
+
+# compare the output of the diff; zero indicates no difference in the diff
+if [[ $? -eq 0 ]]; then
+    echo "success"; 
+    kill %1
+    rm ./tests/result
+    exit 0;
+else 
+    echo "failed"; 
+    kill %1
+    rm ./tests/result
+    exit 1;
+fi
