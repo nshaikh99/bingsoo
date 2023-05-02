6d181b58387f8e5cdd09022d83adc209b4e3b89f
Muthu Palaniappan <muthupal@g.ucla.edu>
2023-05-02 21:27:21 +0000

    Added logic for static file parsing and serving
    #What
    This adds functionality to serve static files to our server under the pathway /static. This also maintains echo functionality under the pathway /echo (for the sample config). All other paths serve a 404 error. the pathway for echo and static are defined in the config file
    
    #Why
    This adds functionality for serving static files. It adds more configurability to the server
    
    #How
    Created new handler.cc and handler.h files as well as edited CMAKE for the static class (similar to how we had done for the echo class). I also added to the config parser class to create functions that look for the static and echo keywords in a config file.
    
    #Testing
    The current test suite passes; I am going to add more unit tests in a follow up CL.
    
    to run:
    `./build/bin/server ./conf/sample.conf` in one terminal from the project directory, and use curl (for ex `curl localhost:80/echo`) in another terminal
    
    Change-Id: Ie3ba97f2b9b3b503f9afea0d52bfeb47e03fdce8

--- src/server_main.cc
+++ src/server_main.cc
@@ -70,6 +70,10 @@ int main(int argc, char* argv[])
     }
 
     int port_num = config.get_port_num();
+    std::vector<std::string> parsed_config_paths = config.get_static_file_path();
+    std::string echo_path = config.get_echo_path();
+    bool is_echo = config.is_echo();
+    bool is_static = config.is_static();
     if (port_num == -1) {
       // cerr << "Invalid port number in config file" << std::endl;
       BOOST_LOG_TRIVIAL(fatal) << LOG_MESSAGE_TYPES[LOG_MESSAGE_TYPE::FATAL] << "Invalid port number in config file";
@@ -80,7 +84,7 @@ int main(int argc, char* argv[])
 
     boost::asio::io_service io_service;
 
-    server s(io_service, port_num); //calls the server::server(...) function in server.cc
+    server s(io_service, port_num, parsed_config_paths, echo_path, is_echo, is_static); //calls the server::server(...) function in server.cc
     BOOST_LOG_TRIVIAL(trace) << LOG_MESSAGE_TYPES[LOG_MESSAGE_TYPE::TRACE] << "Instantiated server";
 
     boost::asio::signal_set signals(io_service, SIGINT, SIGTERM);
