cd36cfd1205401943ddb43cc0c3803d44ae92451
Sabrina Nguyen <shnguyen23@g.ucla.edu>
2023-05-07 19:22:15 +0000

    Changed the parameters for session
    #What:
    Passed in just one NginxConfig config paramater in server and session instead of 4 parameters.
    
    #Why:
    This cleans up the code a little bit and allows us to only intialize one parameter instead of initializing 4 values.
    
    #How:
    I moved the computation of those 4 values into session.cc instead of server_main.cc
    
    #Testing:
    Changed reply_test.cc and session_test.cc test fixtures to permit for only one parameter passed into a new session.
    
    Change-Id: Ia9ba11eb4195c512a6a0439c756363a7202ca8d1

--- tests/session_test.cc
+++ tests/session_test.cc
@@ -1,30 +1,26 @@
 #include "gtest/gtest.h"
 #include "session.h"
-#include "reply.h"
-#include <boost/asio.hpp>
-
+#include <iostream>
 using namespace std;
+
 
 class SessionTestFixture : public ::testing::Test {
   protected:
     boost::asio::io_service io_service;
-    std::vector<std::string> parsed_config_paths;
-    bool is_echo = true;
-    bool is_static = true;
-    std::string echo_path = "/echo";
-    session* new_session = new session(io_service, parsed_config_paths, echo_path, is_echo, is_static);
+    NginxConfigParser config_parser;
+    NginxConfig config;
 };
 
 TEST_F(SessionTestFixture, StartSession){
+  bool parsed_config = config_parser.Parse("./tests/test_configs/static_echo_config", &config);
+  session* new_session = new session(io_service, config);
   EXPECT_TRUE(new_session->start());
 }
 
 TEST_F(SessionTestFixture, ValidRequest)
 {
-//   ostringstream data;
-//   ostream os(data);
-//   string request = "GET / HTTP/1.1\r\nHost: www.test.com\r\nConnection: close\r\n\r\n";
-//   os << request;
+  bool parsed_config = config_parser.Parse("./tests/test_configs/static_echo_config", &config);
+  session* new_session = new session(io_service, config);
   const char* request = "GET / HTTP/1.1\r\nHost: www.test.com\r\nConnection: close\r\n\r\n";
   strcpy(new_session->data_, request);
   int res = new_session->handle_read(boost::system::error_code(), new_session->max_length);
@@ -33,6 +29,8 @@ TEST_F(SessionTestFixture, ValidRequest)
 
 TEST_F(SessionTestFixture, InvalidRequest)
 {
+  bool parsed_config = config_parser.Parse("./tests/test_configs/static_echo_config", &config);
+  session* new_session = new session(io_service, config);
   const char* request = "fake request";
   strcpy(new_session->data_, request);
   int res = new_session->handle_read(boost::system::error_code(), new_session->max_length);
