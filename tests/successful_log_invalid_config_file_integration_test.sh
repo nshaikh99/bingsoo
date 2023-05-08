57fd54a44a5bb210981e9f3a3151ac6efdac7c81
Nabeel Shaikh <nabeelshaikh@g.ucla.edu>
2023-05-08 02:45:30 +0000

    Added static integration test and fixed echo integration test
    Change-Id: Ia520053c0d5da14af37fd398bf0042254f6eb7e7

---
+++ tests/successful_log_invalid_config_file_integration_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Start the webserver; uses bad config with invalid config file
+./build/bin/server asdf &
+
+sleep 1
+
+# check that log records invalid port number
+grep "(Fatal)Invalid config file" ./logs/log_0.log
+
+# compare the output of the grep; zero indicates that the string was found in the log
+if [ $? -eq 0 ]; then
+    echo "success";
+    exit 0;
+else
+    echo "failed";
+    kill %1
+    exit 1;
+fi
