a2d1c38d13f24590e5c3c4af0d05aaae6d56ed00
Isaac Chettiath <ichettiath2019@g.ucla.edu>
2023-05-09 23:15:15 +0000

    Refactored tests in reply_test and echo_request_handler_test
    #WHY: After session.cc and subsequent files were refactored in an earlier commit, the logic in the reply and echo handler tests were disrupted.
    
    #HOW: Added tests for stock_reply function, which is now used in session.cc.
    Change-Id: I88e19f72e9841d596b591213e1e075ab5abddb9e

--- tests/echo_request_handler_test.cc
+++ tests/echo_request_handler_test.cc
@@ -8,10 +8,12 @@ class EchoRequestHandlerTestFixture : public ::testing::Test {
 };
 
 TEST_F(EchoRequestHandlerTestFixture, ValidRequest){
-  char request[1024] = "GET / HTTP/1.1\r\nHost: www.test.com\r\nConnection: close\r\n\r\n";
+  char request[1024] = "sample request";
   reply response = handler.handleRequest(request, strlen(request));
-  bool status_success = response.status == reply::ok ? true : false;
   
-  EXPECT_TRUE(status_success);
-  EXPECT_EQ(request, response.content);
-}
+  bool status_success = response.status == reply::ok;
+  bool content_success = response.content == response.content;
+  bool content_size_success = response.headers[0].value == std::to_string(response.content.size());
+  bool content_type_success = response.headers[1].value == "text/plain";
+  EXPECT_TRUE(status_success && content_success && content_size_success && content_type_success);
+} 
