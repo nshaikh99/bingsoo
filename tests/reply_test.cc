836ef0a406706e56fc98eea4593781cb5621d82b
Isaac Chettiath <ichettiath2019@g.ucla.edu>
2023-05-09 23:15:15 +0000

    Refactored tests in reply_test and echo_request_handler_test
    #WHY: After session.cc and subsequent files were refactored in an earlier commit, the logic in the reply and echo handler tests were disrupted.
    
    #HOW: Added tests for stock_reply function, which is now used in session.cc.
    Change-Id: I88e19f72e9841d596b591213e1e075ab5abddb9e

--- tests/reply_test.cc
+++ tests/reply_test.cc
@@ -3,34 +3,64 @@
 #include "reply.h"
 #include <boost/asio.hpp>
 
+using namespace std;
+
 class ReplyTestFixture : public ::testing::Test {
   protected:
     reply rep;
-    boost::asio::io_service io_service;
-    NginxConfigParser config_parser;
-    NginxConfig config;
-    bool parsed_config = config_parser.Parse("./tests/test_configs/static_echo_config", &config);
-    session* new_session = new session(io_service, config);
-};
+}; 
 
-//TODO: change ReplyTestFixture to reflect the refactored changes made to session.cc
-//WHY: generate_response was removed from session.cc and replaced with reply::stock_reply
+TEST_F(ReplyTestFixture, OkRequest_Stock){
+  rep = reply::stock_reply(reply::ok);
+  bool status_success = rep.status == reply::ok;
+  bool content_success = rep.content == stock_replies::ok;
+  bool content_size_success = rep.headers[0].value == to_string(rep.content.size());
+  bool content_type_success = rep.headers[1].value == "text/html";
+  EXPECT_TRUE(status_success && content_success && content_size_success && content_type_success);
+}
 
-// TEST_F(ReplyTestFixture, ValidRequest) {
-//   char input[1024] = "GET / HTTP/1.1\r\nHost: www.test.com\r\nConnection: close\r\n\r\n";
-//   rep = new_session->generate_response(input, strlen(input), reply::ok);
-//   bool status_success = rep.status == reply::ok ? true : false;
-  
-//   EXPECT_TRUE(status_success);
-//   EXPECT_EQ(input, rep.content);
+TEST_F(ReplyTestFixture, NotFoundRequest_Stock){
+  rep = reply::stock_reply(reply::not_found);
+  bool status_success = rep.status == reply::not_found;
+  bool content_success = rep.content == stock_replies::not_found;
+  bool content_size_success = rep.headers[0].value == to_string(rep.content.size());
+  bool content_type_success = rep.headers[1].value == "text/html";
+  EXPECT_TRUE(status_success && content_success && content_size_success && content_type_success);
+}
+
+TEST_F(ReplyTestFixture, BadRequest_Stock){
+  rep = reply::stock_reply(reply::bad_request);
+  bool status_success = rep.status == reply::bad_request;
+  bool content_success = rep.content == stock_replies::bad_request;
+  bool content_size_success = rep.headers[0].value == to_string(rep.content.size());
+  bool content_type_success = rep.headers[1].value == "text/html";
+  EXPECT_TRUE(status_success && content_success && content_size_success && content_type_success);
+}
+
+//TODO: Finish this buggy test:
+
+// string convert_buffer(vector<boost::asio::const_buffer> buffer){
+//   string res_response = "";
+//   for (int i = 0; i < buffer.size(); i++){
+//     const unsigned char* unsigned_char = static_cast<const unsigned char*> (buffer.at(i).data());
+//     const char* signed_char = (char*) unsigned_char;
+//     string final_char = std::string{signed_char};
+//     res_response += final_char;
+//   }
+//   return res_response;
 // }
 
-// TEST_F(ReplyTestFixture, BadRequest) {
-//   char input[1024] = "fake request";
-//   rep = new_session->generate_response(input, strlen(input), reply::bad_request);
-//   bool status_failure = rep.status == reply::bad_request ? true : false;
-  
-//   EXPECT_TRUE(status_failure);
-//   EXPECT_EQ(input, rep.content);
-// }
+// TEST_F(ReplyTestFixture, ToBuffers){
+//   vector<boost::asio::const_buffer> buffer;
+//   string request = "sample request";
+//   string response_protocol = "HTTP/1.0 200 OK\r\nContent-Length: 14\r\nContent-Type: text/plain\r\n\r\n";
+//   string expected_response = response_protocol + request;
 
+//   rep.content = request;
+//   buffer = rep.to_buffers();
+//   string res_response = convert_buffer(buffer);
+  
+//   bool success = (expected_response == res_response);
+
+//   EXPECT_TRUE(success);
+// }
