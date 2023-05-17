091ab46248df657e31728ca4c6e81779ed82ae5d
Isaac Chettiath <ichettiath2019@g.ucla.edu>
2023-05-17 05:33:30 +0000

    Implement a central place that constructs the handler factories.
    #WHY: By having handlers being constructed from a central place, it makes it easier to add future handlers. It also allows server to have static knowledge of all the available handlers.
    
    #HOW: Created a routes member in session that maps url paths to respective handler factories. routes member is created in server through the createHandlerFactory function. Some tests were adjusted to account for the changes in implementation.
    
    Change-Id: I3b1f6dc4d535c013558c194958b5b9abde3d39c5

--- include/server.h
+++ include/server.h
@@ -2,15 +2,24 @@
 #define server_h
 
 #include "session.h"
+#include "request_handler_factory.h"
+#include "404_handler_factory.h"
+#include "echo_handler_factory.h"
+#include "static_handler_factory.h"
+#include "bad_handler_factory.h"
 
 #include <boost/asio.hpp>
+#include <string>
 
 using boost::asio::ip::tcp;
+using namespace std;
 
 class server
 {
 public:
   server(boost::asio::io_service& io_service, short port, NginxConfig config);
+  RequestHandlerFactory* createHandlerFactory(string name, string uri);
+  std::unordered_map<std::string, RequestHandlerFactory*> configMap();
 private:
   void start_accept();
   void handle_accept(session* new_session, const boost::system::error_code& error);
