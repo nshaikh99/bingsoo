5665bb8022089c61b93044460bf7d927619e04c4
Isaac Chettiath <ichettiath2019@g.ucla.edu>
2023-05-17 05:33:30 +0000

    Implement a central place that constructs the handler factories.
    #WHY: By having handlers being constructed from a central place, it makes it easier to add future handlers. It also allows server to have static knowledge of all the available handlers.
    
    #HOW: Created a routes member in session that maps url paths to respective handler factories. routes member is created in server through the createHandlerFactory function. Some tests were adjusted to account for the changes in implementation.
    
    Change-Id: I3b1f6dc4d535c013558c194958b5b9abde3d39c5

--- tests/reply_test.cc
+++ tests/reply_test.cc
@@ -36,31 +36,3 @@ TEST_F(ReplyTestFixture, BadRequest_Stock){
   bool content_type_success = rep.headers[1].value == "text/html";
   EXPECT_TRUE(status_success && content_success && content_size_success && content_type_success);
 }
-
-//TODO: Finish this buggy test:
-
-// string convert_buffer(vector<boost::asio::const_buffer> buffer){
-//   string res_response = "";
-//   for (int i = 0; i < buffer.size(); i++){
-//     const unsigned char* unsigned_char = static_cast<const unsigned char*> (buffer.at(i).data());
-//     const char* signed_char = (char*) unsigned_char;
-//     string final_char = std::string{signed_char};
-//     res_response += final_char;
-//   }
-//   return res_response;
-// }
-
-// TEST_F(ReplyTestFixture, ToBuffers){
-//   vector<boost::asio::const_buffer> buffer;
-//   string request = "sample request";
-//   string response_protocol = "HTTP/1.0 200 OK\r\nContent-Length: 14\r\nContent-Type: text/plain\r\n\r\n";
-//   string expected_response = response_protocol + request;
-
-//   rep.content = request;
-//   buffer = rep.to_buffers();
-//   string res_response = convert_buffer(buffer);
-  
-//   bool success = (expected_response == res_response);
-
-//   EXPECT_TRUE(success);
-// }
