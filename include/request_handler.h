b8f3d9920b103ccfa21b57287182ccc61b9019b5
Natalia Luzuriaga <nluzuriaga@g.ucla.edu>
2023-05-20 19:23:06 +0000

    Skeleton files & configuration for CrudHandler
    #Why
    Setup needed for CrudHandler
    
    #How
    Created .h and .cc files for CrudHandler with basic class definition.  In config file, CrudHandler is configured to listen for requests at /api & performs crud operations on files in data path which is set to /mnt/crud. Created mnt/crud directory that will serve as the filesystem that CrudHandler will store and retrieve data/files from.
    
    Change-Id: I87672a930c695bd3eeffc43d1079f7d65f678aa3

--- include/request_handler.h
+++ include/request_handler.h
@@ -10,7 +10,7 @@ typedef bool status;
 class RequestHandler { // base class
   public:
     virtual status handle_request(const http::request<http::string_body> req, http::response<http::string_body> &res) = 0; 
-    // implemented by echo_request_handler and static_request_handler
+    // implemented by echo_request_handler, static_request_handler, crud_request_handler
 };
 
 #endif //request_handler_h 
