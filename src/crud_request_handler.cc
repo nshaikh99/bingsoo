0cce97ed53b7f854220acdf66ae6f64409d40537
Dan Lee <danlee01@g.ucla.edu>
2023-05-22 21:59:58 +0000

    Add CRUD POST skeleton to crud_request_handler.cc
    WHY: To set up for implementation of CRUD POST functionality
    
    HOW: Added skeleton in crud_request_handler.cc and comments on how the general approach will be
    
    Change-Id: Idd252065016b1781fd2c8af558137b7f6c35d523

--- src/crud_request_handler.cc
+++ src/crud_request_handler.cc
@@ -3,5 +3,18 @@
 CrudHandler::CrudHandler(std::string path): data_path_(path){}
 
 status CrudHandler::handle_request( const http::request<http::string_body> req, http::response<http::string_body> &res) {
+    if (req.method() == http::verb::post) {
+      // POST request
+
+      // Find next available ID 
+      // Create new instance of Entity ()
+      //  if Entity doesn't exist, create it first? (i.e. make new directory)
+      // Write POST body contents (JSON) to newly created instance
+      // Return ID of newly created instance
+      return false;
+    }
+
+
+    
     return false;
 }
