ce79e09ac80a978fea15919efaafb93269b615ac
Dan Lee <danlee01@g.ucla.edu>
2023-05-23 07:27:41 +0000

    Add CRUD Post functionality
    Change-Id: I255f313239fb623a4a6a0e0e05a57b9b2be6c26a

--- include/crud_request_handler.h
+++ include/crud_request_handler.h
@@ -4,16 +4,19 @@
 #include <string>
 #include "request_handler.h"
 #include <boost/beast/http.hpp>
+#include "filesystem_interface.h"
 
 namespace http = boost::beast::http;
+//namespace fs = boost::filesystem;
 typedef bool status;
 
 class CrudHandler : public RequestHandler {
   public:
-    CrudHandler(std::string path);
+    CrudHandler(std::string path, std::shared_ptr<filesystem_interface> filesystem);
     status handle_request(const http::request<http::string_body> req, http::response<http::string_body> &res);
   private:
     std::string data_path_;
+    std::shared_ptr<filesystem_interface> filesystem_;
 };
 
 #endif // crud_request_handler_h
