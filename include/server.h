efe9ff79d812a6913e344043e0f4bc77162ae256
Isaac Chettiath <ichettiath2019@g.ucla.edu>
2023-05-29 09:36:03 +0000

    Added multithreaded functionality
    Change-Id: I9580b41c8d304fdda0a5568a542e7e2f93f29ce7

--- include/server.h
+++ include/server.h
@@ -23,12 +23,14 @@ public:
   server(boost::asio::io_service& io_service, short port, NginxConfig config);
   RequestHandlerFactory* createHandlerFactory(string name, string uri);
   std::unordered_map<std::string, RequestHandlerFactory*> configMap();
+  void run_threads();
 private:
   void start_accept();
   void handle_accept(session* new_session, const boost::system::error_code& error);
   boost::asio::io_service& io_service_;
   tcp::acceptor acceptor_;
   NginxConfig config_;
+  int thread_count_ = 5;
 };
 
 #endif
