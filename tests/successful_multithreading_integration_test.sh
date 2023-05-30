b30611261b06c1ee114e45304cafcda68a6c487c
Isaac Chettiath <ichettiath2019@g.ucla.edu>
2023-05-30 11:34:56 +0000

    Added integration test for multithreading
    Change-Id: If6a8e27c58c91f7a78f9b9e1fd3a6b0241356d20

---
+++ tests/successful_multithreading_integration_test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Start the webserver; uses port 80
+./build/bin/server ./conf/sample.conf &
+
+sleep 1
+
+curl http://localhost:80/sleep >> ./tests/result & #runs a sleep request in the background
+curl http://localhost:80/echo >> ./tests/result #runs an echo request in the foreground
+
+sleep 2 #wait for sleep request to finish
+
+SLEEP=$( grep -n "Slept" ./tests/result | grep -oE "^[0-9]+" )
+ECHO=$( grep -n "echo" ./tests/result | grep -oE "^[0-9]+" )
+
+rm ./tests/result
+
+# check that "Slept" was found after "echo" in ./tests/result, so we know echo request finished executing
+# while sleep request was still executing even though sleep request was ran first
+if [ $SLEEP -gt $ECHO ] 
+then
+  echo "success";
+  kill %1
+  exit 0;
+else 
+  echo "failure";
+  kill %1
+  exit 1;
+fi
