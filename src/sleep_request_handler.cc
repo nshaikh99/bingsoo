34aca36fc2b4cb833273f49f0f8168a794fb0341
Isaac Chettiath <ichettiath2019@g.ucla.edu>
2023-05-30 11:34:56 +0000

    Added integration test for multithreading
    #WHAT: Added an integration test that verifies when a sleep request is made and an echo request is made shortly after, the echo request finishes executing first.
    
    #WHY: This test is needed to ensure requests are running on different threads.
    
    #HOW: Ran a sleep request in the background then ran an echo request and outputted both to the same file. Verified that the echo request was added to the file first.
    
    Change-Id: If6a8e27c58c91f7a78f9b9e1fd3a6b0241356d20

--- src/sleep_request_handler.cc
+++ src/sleep_request_handler.cc
@@ -2,8 +2,8 @@
 #include <unistd.h>
 
 status SleepHandler::handle_request(const http::request<http::string_body> req, http::response<http::string_body> &res){
-  sleep(3);
-  res.body() = "Slept for 3 second\n";
+  sleep(2);
+  res.body() = "Slept for 2 second\n";
   res.result(http::status::ok);
   res.content_length((res.body().size()));
   res.set(http::field::content_type, "text/plain");
