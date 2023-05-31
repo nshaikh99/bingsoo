fa6372263a3f9994e04c23cf8bde7d98a5a437fc
Muthu Palaniappan <muthupal@g.ucla.edu>
2023-05-31 00:17:33 +0000

    added unit testing for sleep handler and 404 handler to coverage
    #Testing: added two unit tests to increase coverage and make sure the sleep and 404 handler are working correctly
    
    Change-Id: I85e1c668dba2a9714d07cbfe533c82be576e7cc9

---
+++ tests/sleep_request_handler_test.cc
@@ -0,0 +1,34 @@
+#include "gtest/gtest.h"
+#include "sleep_request_handler.h"
+#include "reply.h"
+
+class SleepRequestHandlerTestFixture : public ::testing::Test {
+  protected:
+    SleepHandler handler;
+};
+
+TEST_F(SleepRequestHandlerTestFixture, ValidRequest){
+  http::request<http::string_body> request;
+  request.body() = "Slept for 3 second\n";
+  std::ostringstream request_ostring;
+  request_ostring << request.body();
+  std::string request_string = request_ostring.str();
+
+  http::response<http::string_body> response;
+  handler.handle_request(request, response);
+
+  // Populate an array of strings with the value from the HTTP response so we
+  // can verify these strings match the expected response
+  int idx = 0;
+  std::string fields[2];
+  for (auto& field : response.base()) {
+    fields[idx] = std::string(field.value());
+    idx++;
+  }
+  
+  bool status_success = response.result() == http::status::ok;
+  bool content_success = response.body() == request_string;
+  bool content_size_success = fields[0] == std::to_string(request_string.length());
+  bool content_type_success = fields[1] == "text/plain";
+  EXPECT_TRUE(status_success && content_success && content_size_success && content_type_success);
+} 
