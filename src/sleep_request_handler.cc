45b78da06694bfec01aca6a7b0dce87614bb7c83
Muthu Palaniappan <muthupal@g.ucla.edu>
2023-05-31 06:26:54 +0000

    fixed unit test body after new update
    Change-Id: Ic42f13ac823983c6c626b0cb3ef0eea4ddc6bd93

--- src/sleep_request_handler.cc
+++ src/sleep_request_handler.cc
@@ -3,7 +3,7 @@
 
 status SleepHandler::handle_request(const http::request<http::string_body> req, http::response<http::string_body> &res){
   sleep(2);
-  res.body() = "Slept for 2 second\n";
+  res.body() = "Slept for 2 seconds\n";
   res.result(http::status::ok);
   res.content_length((res.body().size()));
   res.set(http::field::content_type, "text/plain");
--- tests/sleep_request_handler_test.cc
+++ tests/sleep_request_handler_test.cc
@@ -9,13 +9,14 @@ class SleepRequestHandlerTestFixture : public ::testing::Test {
 
 TEST_F(SleepRequestHandlerTestFixture, ValidRequest){
   http::request<http::string_body> request;
-  request.body() = "Slept for 3 second\n";
+  request.body() = "sample request";
   std::ostringstream request_ostring;
   request_ostring << request.body();
   std::string request_string = request_ostring.str();
 
   http::response<http::string_body> response;
   handler.handle_request(request, response);
+  std::string request_content = "Slept for 2 seconds\n";
 
   // Populate an array of strings with the value from the HTTP response so we
   // can verify these strings match the expected response
@@ -27,8 +28,8 @@ TEST_F(SleepRequestHandlerTestFixture, ValidRequest){
   }
   
   bool status_success = response.result() == http::status::ok;
-  bool content_success = response.body() == request_string;
-  bool content_size_success = fields[0] == std::to_string(request_string.length());
+  bool content_success = response.body() == request_content;
+  bool content_size_success = fields[0] == std::to_string(request_content.length());
   bool content_type_success = fields[1] == "text/plain";
   EXPECT_TRUE(status_success && content_success && content_size_success && content_type_success);
 } 
