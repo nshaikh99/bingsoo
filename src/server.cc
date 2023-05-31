833eb0b039956ed2b3909c8ab5956b85fe2ef6f4
Isaac Chettiath <ichettiath2019@g.ucla.edu>
2023-05-31 13:08:50 +0000

    Added unit testing for server
    #WHAT: Added a test file for server.cc
    
    #WHY: Previous iteration had 0 test coverage for server.cc
    
    #HOW: Created the test file server_test.cc
    
    Change-Id: Ic1c937c896bcf0220d78747f1be8d59069ed8d3c

--- src/server.cc
+++ src/server.cc
@@ -22,35 +22,35 @@ void server::run_threads()
 {
   boost::thread_group thread_group;
   for (int i = 0; i < thread_count_; ++i)
-  {
-    thread_group.create_thread(boost::bind(&boost::asio::io_service::run, &io_service_));
-  }
+     thread_group.create_thread(boost::bind(&boost::asio::io_service::run, &io_service_));
   thread_group.join_all();
 }
 
-void server::start_accept()
+bool server::start_accept()
 {
   std::unordered_map<std::string, RequestHandlerFactory*> routes = configMap();
   session* new_session = new session(io_service_, config_, routes); //initializes a tcp socket in member var socket_
+  if (new_session == nullptr)
+    return false;
   acceptor_.async_accept(new_session->socket(), //starts an asynchronous accept for incoming requests 
       boost::bind(&server::handle_accept, this, new_session,
         boost::asio::placeholders::error));
       //bind(f, x, y) returns a function object with the value f(x,y)
+  return true;
 }
 
-void server::handle_accept(session* new_session, //gets called by async_accept each time a request comes in 
+bool server::handle_accept(session* new_session, //gets called by async_accept each time a request comes in 
     const boost::system::error_code& error)
 {
-  if (!error)
-  {
+  if (!error){
     new_session->start(); //session.cc: starts reading from socket stream
   }
-  else
-  {
+  else{
     delete new_session;
+    return false;
   }
-
   start_accept(); //once a request is fully processed, a new session is created to wait for next request
+  return true;
 }
 
 RequestHandlerFactory* server::createHandlerFactory(string name, string uri) {
