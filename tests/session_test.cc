a0671da693334543d212628b545818ea3fd2f9ab
Isaac Chettiath <ichettiath2019@g.ucla.edu>
2023-06-01 17:39:48 +0000

    Added unit test for session.cc
    Change-Id: I68567bc342f6f0f5e265f2b4ec74f835a7764297

--- tests/session_test.cc
+++ tests/session_test.cc
@@ -3,40 +3,34 @@
 #include "echo_handler_factory.h"
 #include <iostream>
 #include <unordered_map>
+#include <strstream>
 using namespace std;
-
+ 
 class SessionTestFixture : public ::testing::Test {
   protected:
-    boost::asio::io_service io_service;
     NginxConfigParser config_parser;
     NginxConfig config;
+    boost::asio::io_service io_service; 
     std::unordered_map<std::string, RequestHandlerFactory*> routes;
+    void SetUp() override 
+    {
+      istrstream config_file("../conf/sample.conf");
+      std::istream* config_path = &config_file;
+      config_parser.Parse(config_path, &config);
+    }
 };
 
 TEST_F(SessionTestFixture, StartSession){
-  bool parsed_config = config_parser.Parse("./tests/test_configs/static_echo_config", &config);
-  session* new_session = new session(io_service, config, routes);
-  EXPECT_TRUE(new_session->start());
+  session* test_session = new session(io_service, config, routes);
+  EXPECT_TRUE(test_session->start());
 }
 
-// TEST_F(SessionTestFixture, ValidRequest)
-// {
-//   bool parsed_config = config_parser.Parse("./tests/test_configs/static_echo_config", &config);
-//   routes["/echo"] = new EchoHandlerFactory("/echo", config);
-//   session* new_session = new session(io_service, config, routes);
-//   new_session.test_routes(routes);
-//   const char* request = "GET / HTTP/1.1\r\nHost: www.test.com\r\nConnection: close\r\n\r\n";
-//   strcpy(new_session->data_, request);
-//   int res = new_session->handle_read(boost::system::error_code(), new_session->max_length);
-//   EXPECT_EQ(res, 0);
-// }
-
-// TEST_F(SessionTestFixture, InvalidRequest)
-// {
-//   bool parsed_config = config_parser.Parse("./tests/test_configs/static_echo_config", &config);
-//   session* new_session = new session(io_service, config, routes);
-//   const char* request = "fake request";
-//   strcpy(new_session->data_, request);
-//   int res = new_session->handle_read(boost::system::error_code(), new_session->max_length);
-//   EXPECT_EQ(res, 1);
-// }
+TEST_F(SessionTestFixture, ValidRequest)
+{
+  routes["/echo"] = new EchoHandlerFactory("/echo", config);
+  session* test_session = new session(io_service, config, routes);
+  const char* request = "GET /echo HTTP/1.1\r\nHost: localhost\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n";
+  strcpy(test_session->data_, request);
+  int res = test_session->handle_read(boost::system::error_code(), test_session->max_length);
+  EXPECT_EQ(res, 0);
+}
