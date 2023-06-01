79dc8ebd0e359b729d4ce6352bb46baa7d219da9
Sabrina Nguyen <shnguyen23@g.ucla.edu>
2023-06-01 20:55:16 +0000

    Created more unit tests
    - Added testing for a List action in CRUD
    - Added testing for Invalid Request, Sleep Request, Health Request, and 404 Not Found Request for session
    
    Change-Id: I52e9310bfac61b4fa9114f385dd48919e3e4d011

--- tests/session_test.cc
+++ tests/session_test.cc
@@ -1,6 +1,9 @@
 #include "gtest/gtest.h"
 #include "session.h"
 #include "echo_handler_factory.h"
+#include "health_handler_factory.h"
+#include "sleep_handler_factory.h"
+#include "404_handler_factory.h"
 #include <iostream>
 #include <unordered_map>
 #include <strstream>
@@ -30,6 +33,46 @@ TEST_F(SessionTestFixture, ValidRequest)
   routes["/echo"] = new EchoHandlerFactory("/echo", config);
   session* test_session = new session(io_service, config, routes);
   const char* request = "GET /echo HTTP/1.1\r\nHost: localhost\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n";
+  strcpy(test_session->data_, request);
+  int res = test_session->handle_read(boost::system::error_code(), test_session->max_length);
+  EXPECT_EQ(res, 0);
+}
+
+TEST_F(SessionTestFixture, InvalidRequest)
+{
+  routes["bad"] = new BadHandlerFactory("bad", config);
+  session* test_session = new session(io_service, config, routes);
+  const char* request = "bad request";
+  strcpy(test_session->data_, request);
+  int res = test_session->handle_read(boost::system::error_code(), test_session->max_length);
+  EXPECT_EQ(res, 1);
+}
+
+TEST_F(SessionTestFixture, HealthRequest)
+{
+  routes["/health"] = new HealthHandlerFactory("/health", config);
+  session* test_session = new session(io_service, config, routes);
+  const char* request = "GET /health HTTP/1.1\r\nHost: localhost\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n";
+  strcpy(test_session->data_, request);
+  int res = test_session->handle_read(boost::system::error_code(), test_session->max_length);
+  EXPECT_EQ(res, 0);
+}
+
+TEST_F(SessionTestFixture, SleepRequest)
+{
+  routes["/sleep"] = new SleepHandlerFactory("/sleep", config);
+  session* test_session = new session(io_service, config, routes);
+  const char* request = "GET /sleep HTTP/1.1\r\nHost: localhost\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n";
+  strcpy(test_session->data_, request);
+  int res = test_session->handle_read(boost::system::error_code(), test_session->max_length);
+  EXPECT_EQ(res, 0);
+}
+
+TEST_F(SessionTestFixture, _404Request)
+{
+  routes["/"] = new _404HandlerFactory("/", config);
+  session* test_session = new session(io_service, config, routes);
+  const char* request = "GET / HTTP/1.1\r\nHost: localhost\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n";
   strcpy(test_session->data_, request);
   int res = test_session->handle_read(boost::system::error_code(), test_session->max_length);
   EXPECT_EQ(res, 0);
