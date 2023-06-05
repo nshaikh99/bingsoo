9ecda93834764e3c9315655079a546a953779ccd
Nabeel Shaikh <nabeelshaikh@g.ucla.edu>
2023-06-05 02:43:13 +0000

    Started implementing markdown handler
    #What
    Started implementing the markdown handler with basic functionality for converting from Markdown to HTML format including:
    - Small, medium, and large headings
    - Regular text
    - Code blocks
    
    #Why
    These are the most commonly used features of Markdown.
    
    #How
    MarkdownHandler::handle_request method reads through Markdown document and converts "# [text]" to "<h1>[text]</h1>", "## [text]" to "<h2>[text]</h2>", and "### [text]" to "<h3>[text]</h3>". Regular "[text]" is converted to "<p>[text]</p>" and "```" is converted to either "<code>" or "</code>" depending on whether or not we are already inside a code block.
    
    #Bugs
    There is currently no support for in-line code, links, bold text, italicized text, underlined text, bulleted lists, and numbered lists. Some (if not all) of these will be added in a later change.
    
    #Testing
    No tests yet. Will be added in a later change.
    
    Change-Id: I1bca5eb514d5a4b30e652d8fc6f67a97780bbf0a

--- src/markdown_handler.cc
+++ src/markdown_handler.cc
@@ -1,9 +1,125 @@
 #include "markdown_handler.h"
 
+#include <boost/algorithm/string/predicate.hpp>
+#include <boost/filesystem.hpp>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
 status MarkdownHandler::handle_request(const http::request<http::string_body> req, http::response<http::string_body> &res) {
-  res.body() = "OK\n";
+  std::string md_file_path = std::string(req.target()).substr(10);
+  
+  // check for .md file extension
+  if(!boost::ends_with(md_file_path, ".md")) {
+    res.body() = "File requested is not a markdown file\n";
+    res.result(http::status::bad_request);
+    res.content_length(res.body().size());
+    res.set(http::field::content_type, "text/plain");
+    return false;
+  }
+
+  // check that file exists
+  if(!boost::filesystem::exists(md_file_path)) {
+    res.body() = "File does not exist\n";
+    res.result(http::status::not_found);
+    res.content_length(res.body().size());
+    res.set(http::field::content_type, "text/plain");
+    return false;
+  }
+
+  std::ifstream md_file(md_file_path);
+  
+  // check that file successfully opened
+  if(!md_file.good()) {
+    res.body() = "Error opening file\n";
+    res.result(http::status::internal_server_error);
+    res.content_length(res.body().size());
+    res.set(http::field::content_type, "text/plain");
+    return false;
+  }
+
+  std::string line;
+  std::vector<std::string> lines;
+
+  // read file
+  while(std::getline(md_file, line)) {
+    lines.push_back(line);
+  }
+  md_file.close();
+
+  // render HTML equivalent
+  std::string html_file_path = md_file_path.substr(0, md_file_path.size() - 3) + ".html";
+
+  // if an HTML equivalent already exists then delete it
+  if(boost::filesystem::exists(html_file_path)) {
+    if(std::remove(&html_file_path[0])) {
+      res.body() = "Error deleting file\n";
+      res.result(http::status::internal_server_error);
+      res.content_length(res.body().size());
+      res.set(http::field::content_type, "text/plain");
+      return false;
+    }
+  }
+  
+  std::ofstream html_file(html_file_path, std::ios_base::app);
+  bool inside_code_snippet = false;
+  for(int i = 0; i < lines.size(); i++) {
+    // small headings
+    if(lines[i].size() >= 4 && lines[i].substr(0, 3) == "###" && lines[i][3] == ' ') {
+      html_file << "<h3 style=\"white-space: pre\">" + lines[i].substr(4) + "</h3>" << std::endl;
+    }
+
+    // medium headings
+    else if(lines[i].size() >= 3 && lines[i].substr(0, 2) == "##" && lines[i][2] == ' ') {
+      html_file << "<h2 style=\"white-space: pre\">" + lines[i].substr(3) + "</h2>" << std::endl;
+    }
+
+    // large headings
+    else if(lines[i].size() >= 2 && lines[i].substr(0, 1) == "#" && lines[i][1] == ' ') {
+      html_file << "<h1 style=\"white-space: pre\">" + lines[i].substr(2) + "</h1>" << std::endl;
+    }
+
+    // block code
+    else if(lines[i].size() >= 3 && lines[i].substr(0, 3) == "```") {
+      if(inside_code_snippet) {
+        inside_code_snippet = !inside_code_snippet;
+        html_file << "</code></p>" << std::endl;
+      }
+      else {
+        inside_code_snippet = !inside_code_snippet;
+        html_file << "<p style=\"white-space: pre\"><code>";
+      }
+    }
+
+    // everything else
+    else {
+      html_file << "<p style=\"white-space: pre\">" + lines[i] + "</p>" << std::endl;
+    }
+  }
+  html_file.close();
+
+  res.body() = "";
+
+  std::ifstream html_file_2(html_file_path);
+  
+  // check that file successfully opened
+  if(!html_file_2.good()) {
+    res.body() = "Error opening file\n";
+    res.result(http::status::internal_server_error);
+    res.content_length(res.body().size());
+    res.set(http::field::content_type, "text/plain");
+    return false;
+  }
+
+  // read file
+  while(std::getline(html_file_2, line)) {
+    res.body() += line;
+  }
+  html_file_2.close();
+  
   res.result(http::status::ok);
   res.content_length(res.body().size());
-  res.set(http::field::content_type, "text/plain");
+  res.set(http::field::content_type, "text/html");
   return true;
 }
