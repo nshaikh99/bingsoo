aec6c478d9a0b0fd3664be40afadf79f6295b202
Sabrina Nguyen <shnguyen23@g.ucla.edu>
2023-06-09 00:40:26 +0000

    Added more unit tests to increase coverage
    Change-Id: I6d6ec7f74f145f985c6a4d31e8167d1921c5e20f

--- tests/session_test.cc
+++ tests/session_test.cc
@@ -1,10 +1,14 @@
 #include "gtest/gtest.h"
 #include "session.h"
+#include "reply.h"
+#include "request_parser.h"
 #include "echo_handler_factory.h"
+#include "static_handler_factory.h"
 #include "health_handler_factory.h"
 #include "sleep_handler_factory.h"
 #include "404_handler_factory.h"
 #include "crud_handler_factory.h"
+#include "markdown_handler_factory.h"
 #include <iostream>
 #include <unordered_map>
 #include <strstream>
@@ -16,6 +20,9 @@ class SessionTestFixture : public ::testing::Test {
     NginxConfig config;
     boost::asio::io_service io_service; 
     std::unordered_map<std::string, RequestHandlerFactory*> routes;
+    request_parser req_parser_;
+    request req_;
+    reply::request_type reply_;
     void SetUp() override 
     {
       istrstream config_file("../conf/sample.conf");
@@ -47,6 +54,16 @@ TEST_F(SessionTestFixture, InvalidRequest)
   strcpy(test_session->data_, request);
   int res = test_session->handle_read(boost::system::error_code(), test_session->max_length);
   EXPECT_EQ(res, 1);
+}
+
+TEST_F(SessionTestFixture, StaticRequest)
+{
+  routes["/static"] = new StaticHandlerFactory("/static", config);
+  session* test_session = new session(io_service, config, routes);
+  const char* request = "GET /static HTTP/1.1\r\nHost: localhost\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n";
+  strcpy(test_session->data_, request);
+  int res = test_session->handle_read(boost::system::error_code(), test_session->max_length);
+  EXPECT_EQ(res, 0);
 }
 
 TEST_F(SessionTestFixture, HealthRequest)
@@ -89,3 +106,24 @@ TEST_F(SessionTestFixture, CrudRequest)
   EXPECT_EQ(res, 0);
 }
 
+TEST_F(SessionTestFixture, MarkdownRequest)
+{
+  routes["/markdown"] = new MarkdownHandlerFactory("/markdown", config);
+  session* test_session = new session(io_service, config, routes);
+  const char* request = "GET /markdown HTTP/1.1\r\nHost: localhost\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n";
+  strcpy(test_session->data_, request);
+  int res = test_session->handle_read(boost::system::error_code(), test_session->max_length);
+  EXPECT_EQ(res, 0);
+} 
+
+// TEST_F(SessionTestFixture, GetRequestType){
+//   routes["/echo"] = new EchoHandlerFactory("/echo", config);
+//   session* test_session = new session(io_service, config, routes);
+//   const char* request = "GET /echo HTTP/1.1\r\nHost: localhost\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n";
+//   strcpy(test_session->data_, request);
+//   int res = test_session->handle_read(boost::system::error_code(), test_session->max_length);
+//   auto pair = req_parser_.parse(req_, test_session->data_, test_session->data_ + test_session->max_length);
+//   req_.uri = "/echo";
+//   reply_ = test_session->get_request_type(req_, config);
+//   EXPECT_TRUE(true);
+// }
